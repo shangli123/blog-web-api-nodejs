932847971a762e116d0463e45357de51379108d6
shangli123 <lishang.good@outlook.com>
2020-09-21 18:38:57 -0600

    add mysql support

---
+++ blog1_withNoFramework/src/config/db.js
@@ -0,0 +1,25 @@
+const env = process.env.NODE_ENV
+
+let MYSQL_CONF
+
+if (env === 'dev') {
+    MYSQL_CONF = {
+        host: 'localhost',
+        user: 'root',
+        password: '1994120912315Ls',
+        port: '3306',
+        database: 'myBlog'
+    }
+}
+
+if  (env === 'production') {
+    MYSQL_CONF = {
+        host: 'localhost',
+        user: 'root',
+        password: '1994120912315Ls',
+        port: '3306',
+        database: 'myBlog'
+    }
+}
+
+module.exports = {MYSQL_CONF}
