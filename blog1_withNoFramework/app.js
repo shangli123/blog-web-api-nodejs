3311907cfffdb14397f90c3c40e13e90a925fe05
shangli123 <lishang.good@outlook.com>
2020-09-26 15:58:52 -0600

    user login

--- blog1_withNoFramework/app.js
+++ blog1_withNoFramework/app.js
@@ -49,11 +49,13 @@ const serverHandle = (req, res) => {
         }
 
         
-        const userData = handleUserRouter(req, res)
-        if (userData) {
-            res.end(
-                JSON.stringify(userData)
-            )
+        const userResult = handleUserRouter(req, res)
+        if (userResult) {
+            userResult.then(userData => {
+                res.end(
+                    JSON.stringify(userData)
+                )
+            })
             return
         }
 
--- blog1_withNoFramework/src/router/user.js
+++ blog1_withNoFramework/src/router/user.js
@@ -8,10 +8,14 @@ const handleUserRouter = (req, res) => {
     if (method == 'POST' && req.path =='/api/user/login') {
         const {username, password} = req.body
         const result = loginCheck(username, password)
-        if (result) {
-            return new SuccessModel()
-        }
-        return new FailModel('Login failed!')
+        return result.then(data => {
+            if (data) {
+                return new SuccessModel()
+            }
+            else {
+                return new FailModel('Login failed!')
+            }
+        })
     }
 
 }
