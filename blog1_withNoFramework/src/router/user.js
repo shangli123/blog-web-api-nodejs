bcc340f7fc4e26c94acd7b6fe6f5feb6a1513a29
shangli123 <lishang.good@outlook.com>
2020-09-27 10:07:26 -0600

    cookie keeps login for 24h

--- blog1_withNoFramework/src/router/user.js
+++ blog1_withNoFramework/src/router/user.js
@@ -1,15 +1,25 @@
 const { login } = require('../controller/user')
 const { SuccessModel, FailModel} = require('../model/resModel')
 
+var getCookieExpires = () => {
+    var d = new Date()
+    d.setTime(d.getTime() + (24*3600*1000))
+    return d.toGMTString()
+}
+
 const handleUserRouter = (req, res) => {
     const method = req.method
 
     // Login Process
-    if (method == 'POST' && req.path =='/api/user/login') {
-        const {username, password} = req.body
+    //if (method == 'POST' && req.path =='/api/user/login') {
+    if (method == 'GET' && req.path =='/api/user/login') {
+        //const {username, password} = req.body
+        const {username, password} = req.query
         const result = login(username, password)
         return result.then(data => {
             if (data) {
+                // Write cookie to browser
+                res.setHeader('Set-Cookie', `username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}` )
                 return new SuccessModel()
             }
             else {
@@ -21,7 +31,7 @@ const handleUserRouter = (req, res) => {
     // Test for login check
     if(method == 'GET' && req.path == '/api/user/login-test') {
         if (req.cookie.username) {
-            return Promise.resolve(new SuccessModel())
+            return Promise.resolve(new SuccessModel(`${req.cookie.username} has already login.` ))
         }
         else {
             return Promise.resolve(new FailModel("No login info detected!"))
